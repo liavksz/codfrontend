8295be01432ec49da2db8364d3c9b24d92b105be
liavksz <alicia_s_souza@estudante.sesisenai.org.br>
2026-09-09 17:12:50 -0300

    primeiro projeto React

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+<div className="cont-app">
+  <h1>trampando com React</h1>
+  <h2>Onde posso trampar com o dev</h2>
+  <p>Desenvolvedor Front-End (Web): Focado na interface com a qual o usuário interage. Trabalharão diretamente com HTML, CSS e JavaScript, além de frameworks modernos (como React, Vue ou Angular).</p>
+<p>Desenvolvedor Back-End: Focado na lógica do servidor, banco de dados e APIs que fazem a aplicação funcionar nos bastidores. Podem atuar com linguagens populares na web (como Node.js, PHP, Python ou Java).</p>
+<p>Desenvolvedor Full-Stack Junior: Para os jovens que demonstram facilidade tanto na interface (Front) quanto na lógica interna (Back), cuidando do ciclo completo de pequenas aplicações.</p>
+<p>Desenvolvedor Mobile (Web/Híbrido): Utilizando frameworks que convertem tecnologias web em aplicativos para celular (como React Native, Flutter ou Ionic), criando apps para Android e iOS.</p>
+<h2>Trampos com tecnologia mas sem programação</h2>
+<ul>
+  <li>Designer Gráfico</li>
+  <li>Analista de Dados</li>
+  <li>Gerente de Projeto</li>
+  <li>Testador de Software</li>
+  <li>Especialista em Suporte Técnico</li>
+</ul>
+</div>
+  )
+}
+
+export default App
