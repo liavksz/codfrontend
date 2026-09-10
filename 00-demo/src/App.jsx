dc9139627dfe589447ed05cb87f597cb114cf3e4
liavksz <alicia_s_souza@estudante.sesisenai.org.br>
2026-09-10 15:35:20 -0300

    tarefa concluida

--- 00-demo/src/App.jsx
+++ 00-demo/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
 <div className="cont-app">
   <h1>trampando com React</h1>
-  <h2>Onde posso trampar com o dev</h2>
+  <h2>Onde posso trampar como dev</h2>
   <p>Desenvolvedor Front-End (Web): Focado na interface com a qual o usuário interage. Trabalharão diretamente com HTML, CSS e JavaScript, além de frameworks modernos (como React, Vue ou Angular).</p>
 <p>Desenvolvedor Back-End: Focado na lógica do servidor, banco de dados e APIs que fazem a aplicação funcionar nos bastidores. Podem atuar com linguagens populares na web (como Node.js, PHP, Python ou Java).</p>
 <p>Desenvolvedor Full-Stack Junior: Para os jovens que demonstram facilidade tanto na interface (Front) quanto na lógica interna (Back), cuidando do ciclo completo de pequenas aplicações.</p>
@@ -17,6 +17,15 @@ function App() {
   <li>Gerente de Projeto</li>
   <li>Testador de Software</li>
   <li>Especialista em Suporte Técnico</li>
+</ul>
+<h2>Vou fazer faculdade de outra coisa, ser dev vai me ajudar?</h2>
+<p>Fazer outra faculdade tendo bagagem de dev não é "jogar conhecimento fora" — é ganhar um superpoder estratégico. No mercado moderno, o profissional mais valioso raramente é o especialista puro de uma única área, mas sim o profissional em "T": alguém com conhecimento profundo em um domínio específico (Sua Nova Faculdade) e alta capacidade técnica transversal (Sua Bagagem de Dev).</p>
+<ul><li>Gestão e Processos (Adm/Engenharia): Você usa lógica algorítmica para desenhar fluxos de trabalho sem gargalos e implementa ferramentas de automação que integram a operação física ao ecossistema digital.</li>
+<li>Dados e Decisão (Marketing/Medicina): Você aplica Python e SQL para cruzar dados complexos — como o comportamento de compra de clientes com dados de bem-estar ou eficiência clínica —, gerando insights preditivos que viram campanhas ou novos serviços.</li>
+<li>Segurança e Viabilidade (Direito): Enquanto projeta essas soluções tecnológicas e campanhas de dados, você garante que tudo nasça em total conformidade com as regras de proteção de dados (LGPD) e segurança da informação, pois entende o código por trás da lei.</li>
+<li>Arquitetura (Cidades e Espaços Inteligentes): Você une o design físico à lógica computacional. Sabendo programar, você domina arquitetura paramétrica (criar formas complexas via código), projeta automação residencial (IoT) real e usa dados urbanos para planejar fluxos de cidades inteligentes.</li>
+<li>Psicologia (Comportamento e IA): Você entende a mente humana e os algoritmos. Isso permite usar ciência de dados para analisar padrões comportamentais em larga escala, desenvolver softwares de suporte terapêutico ou atuar diretamente no treinamento ético e comportamental de Inteligências Artificiais.</li>
+
 </ul>
 </div>
   )
